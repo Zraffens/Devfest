0922cc7f3d2d313596342274b9aa84a28239a792
Abhinav Acharya <abhinavacharya486@gmail.com>
2022-07-20 16:41:00 +0545

    asdf

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -4,13 +4,21 @@
       <h1 class="text-6xl font-bold sm: md:">About</h1>
     </div>
     <div class="block sm:flex align-center">
-      <div class="my-auto left md:flex-1 mr-2">  
-          <img class="rounded-lg" src="../assets/images/img-3.jpeg" alt="">
+      <div class="my-auto left md:flex-1 mr-2">
+        <img class="rounded-lg" src="../assets/images/img-3.jpeg" alt="" />
       </div>
       <div class="md:flex-1 right block mt-8 ml-8">
         <h1 class="block text-4xl font-medium mb-4">About Us</h1>
-        <p class="text-gray-500">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        <p class="text-gray-500">
+          We are a newly founded startup which aims on providing jobs to
+          differently abled people along with the necessary resources. We want
+          to provide an opportunity for differently abled people to learn and
+          earn. We provide different types of jobs for our employees, such as
+          customer service, data entry, content writing and so on. We also have
+          a team of mentors who will guide them through the process of learning
+          and earning in the workplace.
+        </p>
       </div>
-        </div>
     </div>
+  </div>
 </template>
