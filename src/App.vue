8c7ed95c06d7f9da44f56eaa89e8887463d61a4c
Abhinav Acharya <abhinavacharya486@gmail.com>
2022-07-20 10:24:38 +0545

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav>
+    <Navbar />
+  </nav>
+
+  <router-view v-slot="{ Component }">
+    <transition>
+      <component :is="Component"></component>
+    </transition>
+  </router-view>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar.vue'
+export default {
+  name: 'App',
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
